2ef92d375f491c040f4bb5004cf3a37cb0e98794
Povilas udemy app <mobilaizeris@gmail.com>
2016-03-23 14:41:37 +0200

    Error code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,13 +1,18 @@
 class ArticlesController < ApplicationController
+	
+
 	def new 
 		@article = Article.new
 	end
 
 	def create
-		#render plain: params[:article].inspect
 		@article = Article.new(article_params)
-		@article.save	
-		redirect_to articles_show(@article)
+		if @article.save	
+			flash[:notice] = 'Article was succesfully created'
+			redirect_to article_path (@article)
+		else
+			render 'new'
+		end
 	end
 
 	private
