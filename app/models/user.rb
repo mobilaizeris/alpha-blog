6762a798098b2cf503370c8b7629b387e5dfb545
Povilas udemy app <mobilaizeris@gmail.com>
2016-04-09 19:15:35 +0300

    No more errors thank god

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,3 @@
 class User < ActiveRecord::Base
-	validates :username, presence: true, lenght:{minimum: 3, maximum:25}
+	validates :username, presence: true, length:{minimum: 3, maximum:25}
 end
--- db/migrate/20160409142923_drop.rb
+++
@@ -1,5 +0,0 @@
-class Drop < ActiveRecord::Migration
-  def change
-  	#drop_table :users
-  end
-end
