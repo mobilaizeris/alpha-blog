4532781744d5ce9a1a94daa727ccbbe61772c8e3
Povilas udemy app <mobilaizeris@gmail.com>
2016-04-12 18:50:37 +0300

    Add user partials

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,9 +7,23 @@ class UsersController < ApplicationController
 		@user = User.new(user_params)
 		if @user.save
 			flash[:success] = "Welcome to ALPHA-BLOG #{@user.username}"
-			redirect_to articles_path
+			redirect_to articles_path	
 		else
 			render 'new'
+		end
+	end
+
+	def edit
+		@user = User.find(params[:id])
+	end
+
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success] = "Account updated successfully"
+			redirect_to articles_path
+		else
+			render 'edit'
 		end
 	end
 
