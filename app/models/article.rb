5c99fe1135186b4bcd33446612d072cd4a95f16a
Povilas udemy app <mobilaizeris@gmail.com>
2016-05-01 13:43:40 +0300

    Added category and articles associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,7 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
+	has_many :article_categories
+	has_many :categories, through: :article_categories
 	 validates :title, presence: true,length: { minimum: 2, maximum: 30}
 	 validates :description, presence: true, length: { minimum: 4, maximum: 300}
 	 validates :user_id, presence: true
