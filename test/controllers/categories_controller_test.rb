0b077192ea7a94f4aad948dca2accbd38afcf7fe
Povilas udemy app <mobilaizeris@gmail.com>
2016-05-01 13:13:45 +0300

    Fixed last test and updated navbar:

--- test/controllers/categories_controller_test.rb
+++ test/controllers/categories_controller_test.rb
@@ -3,6 +3,7 @@ require "test_helper"
 class CategoriesControllerTest < ActionController::TestCase
 	def setup
 		@category = Category.create(name: "food")
+		@user = User.create(username: "Vardas", email: "nono@gmal.com", password: "labadiena", admin: true)
 	end
 
 	test "should get categories index" do
@@ -11,6 +12,7 @@ class CategoriesControllerTest < ActionController::TestCase
 	end
 
 	test "should get new" do
+		session[:user_id] = @user.id
 		get :new
 		assert_response :success
 	end
@@ -18,5 +20,13 @@ class CategoriesControllerTest < ActionController::TestCase
 	test "should get show" do
 		get(:show, {id: @category.id})
 		assert_response :success
+	end
+
+	test "redirect create if admin is not logged in" do
+		assert_no_difference 'Category.count' do
+			post :create, category: {name: "sports"}
+		end
+		#assert_redirected_to(:controller => "categories", :action => "new")
+		assert_redirected_to categories_path
 	end
 end
