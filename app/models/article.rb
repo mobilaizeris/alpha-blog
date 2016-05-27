4512ebf6929323a1d56a3548986c96eebe1988a9
Povilas <mobilaizeris@gmail.com>
2016-05-27 23:54:35 +0300

    final commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
 	belongs_to :user
 	has_many :article_categories
 	has_many :categories, through: :article_categories
-	 validates :title, presence: true,length: { minimum: 2, maximum: 30}
+	 validates :title, presence: true,length: { minimum: 2, maximum: 50}
 	 validates :description, presence: true, length: { minimum: 4, maximum: 300}
 	 validates :user_id, presence: true
 end
